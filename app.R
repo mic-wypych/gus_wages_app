7fd87ff4762b1ce77e4521c7c884951b8d57953f
MWypych <wypych.mic@gmail.com>
2025-01-14 13:35:49 +0100

    Make the first version of the app
    issues to do:
    - make inputs for filtering work
    - add the table

--- app.R
+++ app.R
@@ -8,6 +8,52 @@
 #
 
 library(shiny)
+library(sf)
+library(ggplot2)
+library(ggiraph)
+library(dplyr)
+library(stringr)
+
+
+powiaty <- read_sf("data/powiaty.shp")
+d <- readxl::read_excel("data/dane_gus_powiat.xlsx", sheet = 2, skip = 1)
+d <- d[-1,]
+d <- d %>%
+    rename("code" = `...1`,
+           "region" = `...2`)
+
+d <- d %>%
+    mutate(across(`2002`:`2023`, .names = "wage_{.col}"))
+
+d_powiat <- d %>%
+    filter(grepl("Powiat.*", region))
+
+
+
+#yay this works!
+#now All we need is to link powiaty to data on wages and
+#then add information with some interactive thing like plotly etc.!
+
+powiaty$JPT_NAZWA_ <- tolower(powiaty$JPT_NAZWA_)
+d_powiat$region <- tolower(d_powiat$region)
+d_powiat$region <- str_remove(d_powiat$region , " m\\. st\\.")
+
+d_powiat$region <- str_remove(d_powiat$region , " m\\.")
+d_powiat$region <- str_remove(d_powiat$region , "powiat")
+powiaty$JPT_NAZWA_ <- str_remove(powiaty$JPT_NAZWA_, "powiat")
+
+d_powiat$region <- trimws(d_powiat$region)
+powiaty$JPT_NAZWA_ <- trimws(powiaty$JPT_NAZWA_)
+
+d_powiat <- d_powiat %>%
+    mutate(region = recode(region,
+                           "wałbrzych od 2013" = "wałbrzych"))
+
+# making the join
+d_powiat_full <- powiaty %>%
+    full_join(d_powiat, by = c("JPT_NAZWA_" = "region"))
+
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -18,32 +64,44 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
+            sliderInput("rok",
+                        "Rok:",
+                        min = 2002,
+                        max = 2023,
+                        value = 2023, 
+                        sep = NULL)
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+           girafeOutput("mapPlot")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
+    
+    
+    
+    
+    output$mapPlot <- renderGirafe({
+        filtered <- paste0("wage_", input$rok)
+        d_powiat_full$wage <- as.numeric(unlist(d_powiat_full[, "wage_2023"]))
+        
+        map_final_interactive <- ggplot(d_powiat_full) +
+            geom_sf_interactive(aes(fill = wage, tooltip = paste0(JPT_NAZWA_,": ",wage))) +
+            scale_fill_gradient(low ="#ccff33", high = "#004b23",
+                                breaks = c(0, 
+                                           round(mean(d_powiat_full$wage, na.rm = T), -3),
+                                           round(max(d_powiat_full$wage, na.rm = T), -3))
+                                ) +
+            labs(fill = "średnia pensja") +
+            theme_void() +
+            theme(legend.position = "top")
+        
+        
+        girafe(ggobj = map_final_interactive)
     })
 }
 
